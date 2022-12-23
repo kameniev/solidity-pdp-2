5494ca715a038e6e162d5add9842664040f6acc6
Ivan Kamenev <ivan.kam@code-care.pro>
2022-12-23 02:10:29 +0100

    change config

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,9 +3,9 @@ require('@nomiclabs/hardhat-waffle');
 module.exports = {
   solidity: '0.8.0',
   networks: {
-    ropsten: {
-      url: '',
-      accounts: [''],
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/XwK8fXA4PNxpEAKnPBoekPWwHfSjcyUV',
+      accounts: ['13c9ca2bf74b39c3572216e5015c3599573fb447d8f3f74f99ef56118f9df7d9'],
     },
   },
 };
